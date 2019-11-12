079a5b03b1523a151fea4f8c4178e0649310269a
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-12 08:49:09 -0300

    main: Add signal monitoring
    This patch adds signal monitoring from interrupt(SIGINT) and
    termination(SIGTERM). It will allow to stop services and close
    connections opened.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/CESARBR/knot-babeltower/internal/config"
 	"github.com/CESARBR/knot-babeltower/pkg/network"
 	"github.com/CESARBR/knot-babeltower/pkg/server"
@@ -8,12 +12,22 @@ import (
 	"github.com/CESARBR/knot-babeltower/pkg/logging"
 )
 
+func monitorSignals(sigs chan os.Signal, logger logging.Logger) {
+	signal := <-sigs
+	logger.Infof("Signal %s received", signal)
+}
+
 func main() {
 	config := config.Load()
 	logrus := logging.NewLogrus(config.Logger.Level)
 
 	logger := logrus.Get("Main")
 	logger.Info("Starting KNoT Babeltower")
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM, syscall.SIGINT)
+
+	go monitorSignals(sigs, logger)
 
 	amqp := network.NewAmqpHandler(config.RabbitMQ.URL, logrus.Get("AmqpHandler"))
 	amqp.Start()
