3fa6e53bfac64454d9d25773c99cde57e1de9c25
Ramon Ribeiro <rhpr@cesar.org.br>
2019-11-21 13:51:58 -0300

    thing: proxy: Add create

--- pkg/network/thing_proxy.go
+++ pkg/network/thing_proxy.go
@@ -1,8 +1,13 @@
 package network
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/CESARBR/knot-babeltower/pkg/entities"
 	"github.com/CESARBR/knot-babeltower/pkg/logging"
 )
 
@@ -17,6 +22,17 @@ type Proxy struct {
 	logger logging.Logger
 }
 
+type errorConflict struct{ error }
+type errorForbidden struct{ error }
+
+func (err errorForbidden) Error() string {
+	return "Error forbidden"
+}
+
+func (err errorConflict) Error() string {
+	return "Error conflict"
+}
+
 // NewThingProxy creates a proxy to the thing service
 func NewThingProxy(logger logging.Logger, hostname string, port uint16) Proxy {
 	url := fmt.Sprintf("http://%s:%d", hostname, port)
@@ -27,6 +43,51 @@ func NewThingProxy(logger logging.Logger, hostname string, port uint16) Proxy {
 
 // SendCreateThing proxy the http request to thing service
 func (p Proxy) SendCreateThing(id, name, authorization string) (idGenerated string, err error) {
+	var resp *http.Response
+	var req *http.Request
+	var uuid, locationHeader string
+
 	p.logger.Debug("Proxying request to create thing")
-	return idGenerated, nil
+	client := &http.Client{Timeout: 10 * time.Second}
+	jsonThing, err := json.Marshal(entities.Thing{ID: id, Name: name})
+	if err != nil {
+		p.logger.Error(err)
+		goto done
+	}
+
+	p.logger.Debug(string(jsonThing))
+
+	req, err = http.NewRequest("POST", p.url+"/things", bytes.NewBuffer(jsonThing))
+	if err != nil {
+		p.logger.Error(err)
+		goto done
+	}
+
+	p.logger.Debug("Authorization header:", authorization)
+	req.Header.Set("Authorization", authorization)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err = client.Do(req)
+	if err != nil {
+		p.logger.Error(err)
+		goto done
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		p.logger.Errorf("Status not created: %d", resp.StatusCode)
+		switch resp.StatusCode {
+		case http.StatusConflict:
+			err = errorConflict{}
+		case http.StatusForbidden:
+			err = errorForbidden{}
+		}
+		goto done
+	}
+
+	locationHeader = resp.Header.Get("Location")
+	uuid = locationHeader[len("/things/"):] // get substring after "/things/"
+	idGenerated = uuid
+done:
+	return idGenerated, err
 }
